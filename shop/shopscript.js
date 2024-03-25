d6238019909c20d74a1ebef509164511fd7bd38e
RazvanConstantin1 <razvanconstantin1097@gmail.com>
2024-03-25 20:48:28 +0200

    fetched the products

--- shop/shopscript.js
+++ shop/shopscript.js
@@ -1,1 +1,25 @@
 "use strict";
+let shop = document.getElementById("shop");
+
+const shopItemsData = fetch("./products.json")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    return (shop.innerHTML = data
+      .map((x) => {
+        let { id, name, price, description, image } = x;
+        return `
+      <div id = product-id-${id} class="item-box">
+      <img class="item-img" src="${image}" alt="product image" />
+      <h3 class="item-name">${name}</h3>
+      <p class="item-description">
+        ${description}
+      </p>
+      <h3 class="item-price"><span>${price} </span> RON</h3>
+    
+      <a class="add-to-cart">Adauga in cos</a>
+    </div>`;
+      })
+      .join(""));
+  })
+  .catch((error) => console.error("Error loading JSON file", error));
