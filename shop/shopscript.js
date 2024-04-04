7a9d7ba356a8c6e4a54ecdd29e80eb6a567fa420
RazvanConstantin1 <razvanconstantin1097@gmail.com>
2024-04-04 20:25:57 +0300

    finished addToCart, update qty and total price

--- shop/shopscript.js
+++ shop/shopscript.js
@@ -11,6 +11,9 @@ const selectors = {
   cart: document.querySelector(".cart"),
   cartOverlay: document.querySelector(".cart-overlay"),
   cartBody: document.querySelector(".cart-content"),
+  cartItem: document.querySelector(".cart-box"),
+  cartTotal: document.querySelector(".total-price"),
+  cartRemove: document.querySelector(".cart-remove"),
 };
 
 // ! Event Listeners
@@ -25,6 +28,7 @@ const setupListeners = () => {
   selectors.cartBtn.addEventListener("click", showCart);
   selectors.cartOverlay.addEventListener("click", hideCart);
   selectors.cartClose.addEventListener("click", hideCart);
+  selectors.cartBody.addEventListener("click", updateCart);
 };
 
 // ! Event Handlers
@@ -60,9 +64,49 @@ const addToCart = (e) => {
   }
 };
 
+const removeFromCart = (id) => {
+  cart = cart.filter((x) => x.id !== id);
+
+  renderProducts();
+};
+
+const increaseQty = (id) => {
+  const item = cart.find((x) => x.id === id);
+  if (!item) return;
+
+  item.qty++;
+};
+const decreaseQty = (id) => {
+  const item = cart.find((x) => x.id === id);
+  if (!item) return;
+
+  item.qty--;
+
+  if (item.qty === 0) removeFromCart(id);
+};
+
+const updateCart = (e) => {
+  if (e.target.hasAttribute("data-btn")) {
+    console.log(e.target);
+    const cartItem = e.target.closest(".cart-box");
+    const id = parseInt(cartItem.dataset.id);
+    console.log(cartItem);
+    const btn = e.target.dataset.btn;
+
+    btn === "incr" && increaseQty(id);
+    btn === "decr" && decreaseQty(id);
+
+    renderCart();
+  }
+};
+
 // ! Render Functions
 
 const renderCart = () => {
+  // show cart total
+  selectors.cartTotal.textContent = calculateTotal();
+
+  // show cart items
   selectors.cartBody.innerHTML = cart
     .map(({ id, qty }) => {
       // get product info for each of cart item
@@ -73,16 +117,21 @@ const renderCart = () => {
       const amount = price * qty;
 
       return `
-      <div class="cart-box">
+      <div class="cart-box" data-id="${id}">
           <img src="${image}" alt="Product photo" class="cart-img">
           <div class="detail-box">
             <div class="cart-product-title">${name}</div>
             <div class="cart-price">${amount} RON</div>
-            <input type="number" value="1" class="cart-quantity">
+            <div class="cart-item-amount">
+                  <i class="bi bi-dash-lg" data-btn="decr"></i>
+                  <span class="qty">${qty}</span>
+                  <i class="bi bi-plus-lg" data-btn="incr"></i>
+                </div>
           </div>
           <!-- Remove cart -->
           <i class="bi bi-trash3 cart-remove"></i>
         </div>
+       
     `;
     })
     .join("");
@@ -97,6 +146,7 @@ const renderProducts = () => {
       const inCart = cart.find((x) => x.id === id);
 
       // Make the add to cart button disabled if already in cart
+
       const disabled = inCart ? "disabled" : "";
 
       // Change the text if already in cart
@@ -108,7 +158,7 @@ const renderProducts = () => {
           <h3 class="item-name">${name}</h3>
           <p class="item-description">${description}</p>
           <h3 class="item-price"><span>${price} </span> RON</h3>
-          <a data-id="${id}" class="add-to-cart">Adauga in cos</a>
+          <a data-id="${id}" class="add-to-cart ${disabled}">${text}</a>
         </div>
       `;
     })
@@ -131,6 +181,18 @@ const loadProducts = async (apiURL) => {
 };
 
 // ! Helper Functions
+
+const calculateTotal = () => {
+  return cart
+    .map(({ id, qty }) => {
+      const { price } = products.find((x) => x.id === id);
+
+      return qty * price;
+    })
+    .reduce((sum, number) => {
+      return sum + number;
+    }, 0);
+};
 
 // ! INITIALIZE
 
