edef5cc21a08974eb2dd04945a3ee31f002f3aac
RazvanConstantin1 <razvanconstantin1097@gmail.com>
2024-04-18 20:39:44 +0300

    removed the sticky nav in shop page

--- shop/shopscript.js
+++ shop/shopscript.js
@@ -2,32 +2,32 @@
 let products = [];
 let cart = [];
 
-// ! Sticky Navigation
-// Sticky navigation
-const sectionBackgroundEl = document.querySelector(".section-background");
+// // ! Sticky Navigation
+// // Sticky navigation
+// const sectionBackgroundEl = document.querySelector(".section-background");
 
-const obs = new IntersectionObserver(
-  function (entries) {
-    const ent = entries[0];
-    console.log(ent);
+// const obs = new IntersectionObserver(
+//   function (entries) {
+//     const ent = entries[0];
+//     console.log(ent);
 
-    if (ent.isIntersecting === false) {
-      document.body.classList.add("sticky");
-    }
+//     if (ent.isIntersecting === false) {
+//       document.body.classList.add("sticky");
+//     }
 
-    if (ent.isIntersecting === true) {
-      document.body.classList.remove("sticky");
-    }
-  },
+//     if (ent.isIntersecting === true) {
+//       document.body.classList.remove("sticky");
+//     }
+//   },
 
-  // In the viewport
-  {
-    root: null,
-    threshold: 0,
-    rootMargin: "-80px",
-  }
-);
-obs.observe(sectionBackgroundEl);
+//   // In the viewport
+//   {
+//     root: null,
+//     threshold: 0,
+//     rootMargin: "-80px",
+//   }
+// );
+// obs.observe(sectionBackgroundEl);
 
 // ! Selectors
 const selectors = {
