d8d4a98af028f2db3e7b4d83f603832c16a0ca52
RazvanConstantin1 <razvanconstantin1097@gmail.com>
2024-06-22 14:23:10 +0200

    solved the scrolling bug and adjusted products section css

--- script.js
+++ script.js
@@ -75,16 +75,18 @@ const revealSection = function (entries, observer) {
 
   if (!entry.isIntersecting) return;
   entry.target.classList.remove("section--hidden");
+  entry.target.style.overflow = "hidden";
   observer.unobserve(entry.target);
 };
 
 const sectionObserver = new IntersectionObserver(revealSection, {
   root: null,
-  threshold: 0.02,
+  threshold: 0.03,
 });
 allSections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add("section--hidden");
+  document.body.style.overflow = "hidden";
 });
 
 // Loading imgs
